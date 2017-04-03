cce1fc6b8ad743ab9b3c19cb26f130f08869fe52
Brandon Yeager <byeager@bstsoftwarecompany.com>
2017-04-03 17:10:46 -0500

    Replace default code with SimpleClass definition

--- ClarionDLL/ClarionDLL.h
+++ ClarionDLL/ClarionDLL.h
@@ -2,12 +2,23 @@
 
 #pragma once
 
+#include <msclr\auto_gcroot.h>
+
 using namespace System;
+using namespace msclr;
 
-namespace ClarionDLL {
 
-	public ref class Class1
-	{
-		// TODO: Add your methods for this class here.
-	};
-}
+public class SimpleClass
+{
+public:
+	void setIntValue(int newIntValue);
+	int getIntValue();
+	void setDoubleValue(double newDoubleValue);
+	double getDoubleValue();
+	void setStringValue(String ^ newStringValue);
+	String ^ getStringValue();
+private:
+	int intValue;
+	double doubleValue;
+	auto_gcroot<String ^> stringValue;
+};
