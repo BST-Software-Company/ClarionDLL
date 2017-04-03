aed41a63f7c0103200857b1dbfdfcc4c09b8e1a0
Brandon Yeager <byeager@bstsoftwarecompany.com>
2017-04-03 17:12:02 -0500

    Implement SimpleClass methods

--- ClarionDLL/ClarionDLL.cpp
+++ ClarionDLL/ClarionDLL.cpp
@@ -4,3 +4,33 @@
 
 #include "ClarionDLL.h"
 
+
+void SimpleClass::setIntValue(int newIntValue)
+{
+	intValue = newIntValue;
+}
+
+int SimpleClass::getIntValue()
+{
+	return intValue;
+}
+
+void SimpleClass::setDoubleValue(double newDoubleValue)
+{
+	doubleValue = newDoubleValue;
+}
+
+double SimpleClass::getDoubleValue()
+{
+	return doubleValue;
+}
+
+void SimpleClass::setStringValue(String ^ newStringValue)
+{
+	stringValue = newStringValue;
+}
+
+String ^ SimpleClass::getStringValue()
+{
+	return stringValue.get();
+}
